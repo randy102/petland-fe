da77e5045e25d9b7e6b90c939e5059ed6b9b8558
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-03-19 20:47:38 +0700

    Add Typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { CssBaseline } from '@material-ui/core';
+import React from 'react';
+import './App.css';
+import Default from './default';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <Default />
+    </React.Fragment>
+  );
+}
+
+export default App;
