3d10ff4fe01cd52c9f55bd3eecf922285d52f64b
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-03-31 20:13:18 +0700

    Add submit handler for search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,23 +1,37 @@
-import useStyles from './styles'
-import SearchIcon from '@material-ui/icons/Search'
 import { InputBase } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import { useForm } from 'react-hook-form'
+import useStyles from './styles'
 import TypeSelect from './TypeSelect'
 
 export default function SearchBar() {
   const classes = useStyles()
 
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = handleSubmit(data => {
+    console.log('Keyword:', data.search)
+  })
+
   return (
-    <div className={classes.search}>
+    <form
+      noValidate
+      className={classes.search}
+      onSubmit={onSubmit}
+    >
       <SearchIcon />
 
       <InputBase
         classes={{
           root: classes.inputRoot
         }}
+        defaultValue=""
+        inputRef={register}
+        name="search"
         placeholder="Tìm kiếm..."
       />
 
       <TypeSelect />
-    </div>
+    </form>
   )
 }
